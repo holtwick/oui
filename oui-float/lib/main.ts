04cf1d7dbe43613f60a3f110282aaee5cd464dd4
Dirk Holtwick <dirk.holtwick@gmail.com>
2024-01-16 18:01:37 +0100

    fix

--- oui-float/lib/main.ts
+++ oui-float/lib/main.ts
@@ -1,5 +1,7 @@
 import './css.styl'
 
+export * from './_types'
+
 export { default as OuiFloat } from './oui-float.vue'
 export { default as OuiMenu } from './oui-menu.vue'
 export { default as OuiTooltipActivator } from './oui-tooltip-activator.vue'
--- oui-notification/lib/main.ts
+++ oui-notification/lib/main.ts
@@ -1,5 +1,6 @@
 import './css.styl'
 
+export * from './_types'
 export * from './notification'
 
 export { default as OuiNotificationActivator } from './oui-notification-activator.vue'
