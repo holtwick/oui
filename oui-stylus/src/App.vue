903e5f4eb967b3dadfd727bbe7f55bca62b7182a
Dirk Holtwick <dirk.holtwick@gmail.com>
2024-02-02 15:52:16 +0100

    demo

--- oui-stylus/src/App.vue
+++ oui-stylus/src/App.vue
@@ -1,14 +1,37 @@
 <script setup lang="ts">
-import AppDemo from './app-demo.vue'
+import { useLocalStorage } from '@vueuse/core'
+import { computed, ref } from 'vue'
+
+const modes = import.meta.glob('./app-*.vue', {
+  import: 'default',
+  eager: true,
+})
+
+const mode = useLocalStorage('oui.mode', './app-text.vue')
+const dark = useLocalStorage('oui.dark', true)
+
+const comp = computed(() => modes[mode.value])
 </script>
 
 <template>
-  <div class="_stack_x" style="gap: 16px">
+  <div class="app-demo">
+    <select v-model="mode">
+      <template v-for="value in Object.keys(modes)" :key="value">
+        <option :value="value">
+          {{ value }}
+        </option>
+      </template>
+    </select>
+    &nbsp;
+    <input v-model="dark" type="checkbox"> Show Dark Mode Preview
+  </div>
+
+  <div v-if="comp" class="_stack_x" style="gap: 16px; width: 100%;">
     <div>
-      <AppDemo />
+      <component :is="comp" />
     </div>
-    <div class="dark default">
-      <AppDemo />
+    <div v-if="dark" class="dark default">
+      <component :is="comp" />
     </div>
   </div>
 </template>
