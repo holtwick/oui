5e1a8dd853f3a4cb273f2803c1c971fc0d1fdbda
Dirk Holtwick <dirk.holtwick@gmail.com>
2024-02-28 18:23:33 +0100

    just a basic example

---
+++ oui-demo/src/stories/OuiFloat.story.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+import { logEvent } from 'histoire/client'
+import { ref } from 'vue'
+import { OuiFloat } from 'oui-float'
+
+import 'oui-float/css'
+
+function initialState() {
+  return {
+    show: false,
+  }
+}
+
+const button = ref()
+const show = ref(false)
+</script>
+
+<template>
+  <Story auto-props-disabled>
+    <template #controls="{ state }">
+      <HstCheckbox v-model="state.show" title="show" />
+    </template>
+    <Variant title="Simple popover" :init-state="initialState">
+      <template #default="{ state }">
+        <button ref="button" @click="state.show = !state.show">
+          Click me
+        </button>
+        <OuiFloat v-model="state.show" :reference="button">
+          This is the popover
+        </OuiFloat>
+      </template>
+    </Variant>
+  </story>
+</template>
