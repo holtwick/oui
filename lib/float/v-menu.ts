8c01e4fee8d2ffcfae0b088e4e8d7f6ebee82eb2
Dirk Holtwick <dirk.holtwick@gmail.com>
2024-03-07 11:26:13 +0100

    chore: keep it simple

--- lib/float/v-menu.ts
+++ lib/float/v-menu.ts
@@ -4,44 +4,20 @@ import { Logger } from 'zeed'
 
 const log: LoggerInterface = Logger('oui:v-menu')
 
-function g() {
-  return globalThis as any
-}
-
-function getMenuElements() {
-  let menuElements = g().__ouiVMenuCache
-  if (menuElements == null) {
-    menuElements = new Map<HTMLElement, any>()
-    g().__ouiVMenuCache = menuElements
-  }
-  return menuElements
-}
-
 export const vMenu = {
-  updated: (element: HTMLElement, binding: DirectiveBinding) => {
-    // log('v-menu:updated', element, binding)
+  updated: (element: any, binding: DirectiveBinding) => {
     log.assert(typeof binding.value === 'function', 'v-menu requires function as argument')
-    getMenuElements().set(element, binding.value)
+    element.__ouiVMenu = binding.value
   },
-  mounted: (element: HTMLElement, binding: DirectiveBinding) => {
-    // log('v-menu:mounted', element, binding)
+  mounted: (element: any, binding: DirectiveBinding) => {
     log.assert(typeof binding.value === 'function', 'v-menu requires function as argument')
-
-    getMenuElements().set(element, binding.value)
+    element.__ouiVMenu = binding.value
     element.addEventListener('contextmenu', (event: MouseEvent) => {
-      // log('v-menu context')
       event.preventDefault() // no system menu
-      const fn = getMenuElements().get(element)
-      fn?.(event, element)
+      element.__ouiVMenu?.(event, element)
     })
     element.addEventListener('click', (event: MouseEvent) => {
-      // log('v-menu click')
-      const fn = getMenuElements().get(element)
-      fn?.(event, element)
+      element.__ouiVMenu?.(event, element)
     })
-  },
-  beforeUnmount: (element: HTMLElement, binding: DirectiveBinding) => {
-    // log('v-menu:beforeUnmount', element, binding)
-    getMenuElements().delete(element)
   },
 }
