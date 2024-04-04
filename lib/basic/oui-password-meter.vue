10fb38fe8a0cc9636e1a0f32d929528ae6dd55f1
Dirk Holtwick <dirk.holtwick@gmail.com>
2024-04-05 00:40:05 +0200

    feat: oui-password

---
+++ lib/basic/oui-password-meter.vue
@@ -0,0 +1,64 @@
+<script lang="ts" setup>
+import { computed } from 'vue'
+
+import './oui-password.styl'
+
+const props = defineProps<{
+  value?: string
+}>()
+
+const score = computed(() => scorePassword(props.value ?? ''))
+
+function scorePassword(pass: string) {
+  let score = 0
+  let length = 0
+  let specialChar = 0
+  let caseMix = 0
+  let numCharMix = 0
+
+  const specialCharRegex = /[^A-Za-z0-9]/g
+  const lowercaseRegex = /(.*[a-z].*)/g
+  const uppercaseRegex = /(.*[A-Z].*)/g
+  const numberRegex = /(.*[0-9].*)/g
+  const repeatCharRegex = /(\w)(\1+\1+\1+\1+)/g
+
+  const hasSpecialChar = specialCharRegex.test(pass)
+  const hasLowerCase = lowercaseRegex.test(pass)
+  const hasUpperCase = uppercaseRegex.test(pass)
+  const hasNumber = numberRegex.test(pass)
+  const hasRepeatChars = repeatCharRegex.test(pass)
+
+  if (pass.length > 4) {
+    if ((hasLowerCase || hasUpperCase) && hasNumber)
+      numCharMix = 1
+
+    if (hasUpperCase && hasLowerCase)
+      caseMix = 1
+
+    if ((hasLowerCase || hasUpperCase || hasNumber) && hasSpecialChar)
+      specialChar = 1
+
+    if (pass.length > 8)
+      length = 1
+
+    if (pass.length > 12 && !hasRepeatChars)
+      length = 2
+
+    if (pass.length > 25 && !hasRepeatChars)
+      length = 3
+
+    score = length + specialChar + caseMix + numCharMix
+
+    if (score > 4)
+      score = 4
+  }
+
+  return score
+}
+</script>
+
+<template>
+  <div>
+    <div class="oui-password-strength" :data-score="score" />
+  </div>
+</template>
