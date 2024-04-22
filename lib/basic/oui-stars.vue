d482e842a18d87157e0c51ba4038e6b2df52b942
Dirk Holtwick <dirk.holtwick@gmail.com>
2024-04-22 16:45:48 +0200

    add some docs

--- lib/basic/oui-stars.vue
+++ lib/basic/oui-stars.vue
@@ -2,10 +2,9 @@
 import { ref } from 'vue'
 import './oui-stars.styl'
 
-withDefaults(defineProps<{
-  title?: string
-}>(), {
-})
+// withDefaults(defineProps<{
+// }>(), {
+// })
 
 const model = defineModel({ required: true, default: 0 })
 
@@ -16,9 +15,11 @@ const hover = ref(0)
   <div class="oui-stars">
     <template v-for="i in 5" :key="i">
       <div :class="{ _active: model >= i, _hover: hover >= i }" @click="model = i" @mouseover="hover = i">
-        <svg viewBox="0 0 512 512" fill="currentColor">
-          <path d="M394 480a16 16 0 01-9.39-3L256 383.76 127.39 477a16 16 0 01-24.55-18.08L153 310.35 23 221.2a16 16 0 019-29.2h160.38l48.4-148.95a16 16 0 0130.44 0l48.4 149H480a16 16 0 019.05 29.2L359 310.35l50.13 148.53A16 16 0 01394 480z" />
-        </svg>
+        <slot name="icon">
+          <svg viewBox="0 0 512 512" fill="currentColor">
+            <path d="M394 480a16 16 0 01-9.39-3L256 383.76 127.39 477a16 16 0 01-24.55-18.08L153 310.35 23 221.2a16 16 0 019-29.2h160.38l48.4-148.95a16 16 0 0130.44 0l48.4 149H480a16 16 0 019.05 29.2L359 310.35l50.13 148.53A16 16 0 01394 480z" />
+          </svg>
+        </slot>
       </div>
     </template>
   </div>
