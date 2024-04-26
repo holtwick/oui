059043a2406bd0c2e3303f6e973442b325991323
Dirk Holtwick <dirk.holtwick@gmail.com>
2024-04-26 17:54:21 +0200

    feat: get rid of histoire

--- histoire.setup.ts
+++
@@ -1,8 +0,0 @@
-import { defineSetupVue3 } from '@histoire/plugin-vue'
-import WrapperGlobal from './src/histoire-wrapper.vue'
-
-import './stylus/default-story.styl'
-
-export const setupVue3 = defineSetupVue3(({ app, addWrapper }) => {
-  addWrapper(WrapperGlobal)
-})
