d8c0e8617b1f74a04ff1534f140557e64595ba2e
Dirk Holtwick <dirk.holtwick@gmail.com>
2024-05-18 18:47:13 +0200

    catch most scrolling issues

--- src/app-mobile.vue
+++ src/app-mobile.vue
@@ -1,148 +1,86 @@
 <script lang="ts" setup>
 import type { LoggerInterface } from 'zeed'
 import { Logger } from 'zeed'
-
+import { onMounted, reactive, ref } from 'vue'
+import { useEventListener } from '@vueuse/core'
+import { vNoSelection } from '../lib/basic'
 import './app-mobile.styl'
 
 const log: LoggerInterface = Logger('app-mobile')
+
+const info = reactive<any>({ })
+
+if (window.visualViewport != null) {
+  function resizeHandler() {
+    info.height = window.visualViewport?.height
+    info.top = window.visualViewport?.offsetTop
+
+    const visibleHeight = `${window.visualViewport?.height.toString()}px`
+    const visibleOffsetTop = `${window.visualViewport?.offsetTop.toString()}px`
+
+    // Adjust the height!
+    document.documentElement.style.height = visibleHeight
+
+    log('new height', visibleHeight, window.visualViewport)
+    // rootCss.setProperty('--visible-height', visibleHeight)
+    // rootCss.setProperty('--visible-offset-top', visibleOffsetTop)
+    // window.scrollTo(0, 0)
+  }
+
+  function scrollHandler() {
+    info.scrollY = window.screenY
+    resizeHandler()
+  }
+
+  useEventListener(window.visualViewport, 'resize', resizeHandler)
+  useEventListener(window.visualViewport, 'scroll', scrollHandler)
+
+  useEventListener(window, 'touchmove', (ev) => {
+    let el = ev.target as HTMLElement | undefined | null
+    info.move = el?.outerHTML.slice(0, 20)
+
+    while (el != null) {
+      if (el.dataset.scroll)
+        return
+      el = el.parentElement
+    }
+
+    ev.preventDefault()
+    ev.stopPropagation()
+  }, {
+    passive: false,
+    capture: true,
+  })
+
+  // app.addEventListener('touchmove', (ev: MouseEvent) => {
+  //   console.log('move', ev.target)
+  //   ev.preventDefault()
+  //   ev.stopPropagation()
+  //   window.scrollTo(0, 0)
+  // }, {
+  //   passive: false,
+  //   capture: true,
+  // })
+
+  onMounted(resizeHandler)
+}
 </script>
 
 <template>
   <div class="app-mobile">
     <header>
-      Header
+      Header {{ info }}
     </header>
-    <main>
-      <OuiText>
-        <ol>
+    <main data-scroll="true">
+      <ol>
+        <template v-for="i in 100" :key="i">
           <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus at voluptatem, optio nihil facere dolorum, porro excepturi ducimus cupiditate, odio officiis laboriosam exercitationem tempore voluptas repellat corporis ex accusamus iste!
+            <input type="text" value="Hallo">
+            <!-- <OuiInput v-model="text" title="fasdfd" /> -->
+            {{ i }}  Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus at voluptatem, optio nihil facere dolorum, porro excepturi ducimus cupiditate, odio officiis laboriosam exercitationem tempore voluptas repellat corporis ex accusamus iste!
           </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus at voluptatem, optio nihil facere dolorum, porro excepturi ducimus cupiditate, odio officiis laboriosam exercitationem tempore voluptas repellat corporis ex accusamus iste!
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus at voluptatem, optio nihil facere dolorum, porro excepturi ducimus cupiditate, odio officiis laboriosam exercitationem tempore voluptas repellat corporis ex accusamus iste!
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus at voluptatem, optio nihil facere dolorum, porro excepturi ducimus cupiditate, odio officiis laboriosam exercitationem tempore voluptas repellat corporis ex accusamus iste!
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus at voluptatem, optio nihil facere dolorum, porro excepturi ducimus cupiditate, odio officiis laboriosam exercitationem tempore voluptas repellat corporis ex accusamus iste!
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus at voluptatem, optio nihil facere dolorum, porro excepturi ducimus cupiditate, odio officiis laboriosam exercitationem tempore voluptas repellat corporis ex accusamus iste!
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus at voluptatem, optio nihil facere dolorum, porro excepturi ducimus cupiditate, odio officiis laboriosam exercitationem tempore voluptas repellat corporis ex accusamus iste!
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus at voluptatem, optio nihil facere dolorum, porro excepturi ducimus cupiditate, odio officiis laboriosam exercitationem tempore voluptas repellat corporis ex accusamus iste!
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus at voluptatem, optio nihil facere dolorum, porro excepturi ducimus cupiditate, odio officiis laboriosam exercitationem tempore voluptas repellat corporis ex accusamus iste!
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus at voluptatem, optio nihil facere dolorum, porro excepturi ducimus cupiditate, odio officiis laboriosam exercitationem tempore voluptas repellat corporis ex accusamus iste!
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus at voluptatem, optio nihil facere dolorum, porro excepturi ducimus cupiditate, odio officiis laboriosam exercitationem tempore voluptas repellat corporis ex accusamus iste!
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus at voluptatem, optio nihil facere dolorum, porro excepturi ducimus cupiditate, odio officiis laboriosam exercitationem tempore voluptas repellat corporis ex accusamus iste!
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus at voluptatem, optio nihil facere dolorum, porro excepturi ducimus cupiditate, odio officiis laboriosam exercitationem tempore voluptas repellat corporis ex accusamus iste!
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus at voluptatem, optio nihil facere dolorum, porro excepturi ducimus cupiditate, odio officiis laboriosam exercitationem tempore voluptas repellat corporis ex accusamus iste!
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus at voluptatem, optio nihil facere dolorum, porro excepturi ducimus cupiditate, odio officiis laboriosam exercitationem tempore voluptas repellat corporis ex accusamus iste!
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus at voluptatem, optio nihil facere dolorum, porro excepturi ducimus cupiditate, odio officiis laboriosam exercitationem tempore voluptas repellat corporis ex accusamus iste!
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus at voluptatem, optio nihil facere dolorum, porro excepturi ducimus cupiditate, odio officiis laboriosam exercitationem tempore voluptas repellat corporis ex accusamus iste!
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus at voluptatem, optio nihil facere dolorum, porro excepturi ducimus cupiditate, odio officiis laboriosam exercitationem tempore voluptas repellat corporis ex accusamus iste!
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus at voluptatem, optio nihil facere dolorum, porro excepturi ducimus cupiditate, odio officiis laboriosam exercitationem tempore voluptas repellat corporis ex accusamus iste!
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus at voluptatem, optio nihil facere dolorum, porro excepturi ducimus cupiditate, odio officiis laboriosam exercitationem tempore voluptas repellat corporis ex accusamus iste!
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus at voluptatem, optio nihil facere dolorum, porro excepturi ducimus cupiditate, odio officiis laboriosam exercitationem tempore voluptas repellat corporis ex accusamus iste!
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus at voluptatem, optio nihil facere dolorum, porro excepturi ducimus cupiditate, odio officiis laboriosam exercitationem tempore voluptas repellat corporis ex accusamus iste!
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus at voluptatem, optio nihil facere dolorum, porro excepturi ducimus cupiditate, odio officiis laboriosam exercitationem tempore voluptas repellat corporis ex accusamus iste!
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus at voluptatem, optio nihil facere dolorum, porro excepturi ducimus cupiditate, odio officiis laboriosam exercitationem tempore voluptas repellat corporis ex accusamus iste!
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus at voluptatem, optio nihil facere dolorum, porro excepturi ducimus cupiditate, odio officiis laboriosam exercitationem tempore voluptas repellat corporis ex accusamus iste!
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus at voluptatem, optio nihil facere dolorum, porro excepturi ducimus cupiditate, odio officiis laboriosam exercitationem tempore voluptas repellat corporis ex accusamus iste!
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus at voluptatem, optio nihil facere dolorum, porro excepturi ducimus cupiditate, odio officiis laboriosam exercitationem tempore voluptas repellat corporis ex accusamus iste!
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus at voluptatem, optio nihil facere dolorum, porro excepturi ducimus cupiditate, odio officiis laboriosam exercitationem tempore voluptas repellat corporis ex accusamus iste!
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus at voluptatem, optio nihil facere dolorum, porro excepturi ducimus cupiditate, odio officiis laboriosam exercitationem tempore voluptas repellat corporis ex accusamus iste!
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus at voluptatem, optio nihil facere dolorum, porro excepturi ducimus cupiditate, odio officiis laboriosam exercitationem tempore voluptas repellat corporis ex accusamus iste!
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus at voluptatem, optio nihil facere dolorum, porro excepturi ducimus cupiditate, odio officiis laboriosam exercitationem tempore voluptas repellat corporis ex accusamus iste!
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus at voluptatem, optio nihil facere dolorum, porro excepturi ducimus cupiditate, odio officiis laboriosam exercitationem tempore voluptas repellat corporis ex accusamus iste!
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus at voluptatem, optio nihil facere dolorum, porro excepturi ducimus cupiditate, odio officiis laboriosam exercitationem tempore voluptas repellat corporis ex accusamus iste!
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus at voluptatem, optio nihil facere dolorum, porro excepturi ducimus cupiditate, odio officiis laboriosam exercitationem tempore voluptas repellat corporis ex accusamus iste!
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus at voluptatem, optio nihil facere dolorum, porro excepturi ducimus cupiditate, odio officiis laboriosam exercitationem tempore voluptas repellat corporis ex accusamus iste!
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus at voluptatem, optio nihil facere dolorum, porro excepturi ducimus cupiditate, odio officiis laboriosam exercitationem tempore voluptas repellat corporis ex accusamus iste!
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus at voluptatem, optio nihil facere dolorum, porro excepturi ducimus cupiditate, odio officiis laboriosam exercitationem tempore voluptas repellat corporis ex accusamus iste!
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus at voluptatem, optio nihil facere dolorum, porro excepturi ducimus cupiditate, odio officiis laboriosam exercitationem tempore voluptas repellat corporis ex accusamus iste!
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus at voluptatem, optio nihil facere dolorum, porro excepturi ducimus cupiditate, odio officiis laboriosam exercitationem tempore voluptas repellat corporis ex accusamus iste!
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus at voluptatem, optio nihil facere dolorum, porro excepturi ducimus cupiditate, odio officiis laboriosam exercitationem tempore voluptas repellat corporis ex accusamus iste!
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus at voluptatem, optio nihil facere dolorum, porro excepturi ducimus cupiditate, odio officiis laboriosam exercitationem tempore voluptas repellat corporis ex accusamus iste!
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus at voluptatem, optio nihil facere dolorum, porro excepturi ducimus cupiditate, odio officiis laboriosam exercitationem tempore voluptas repellat corporis ex accusamus iste!
-          </li>
-        </ol>
-      </OuiText>
+        </template>
+      </ol>
     </main>
     <footer>
       Footer
