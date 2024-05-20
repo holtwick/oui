6fc7b4ab508ed615239f580b5f4e66a87c699219
Dirk Holtwick <dirk.holtwick@gmail.com>
2024-05-20 10:55:08 +0200

    experiments

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import { createApp } from 'vue'
 // import('./app-mobile.vue').then(app => createApp(app.default).mount('#app'))
 // import('./app.vue').then(app => createApp(app.default).mount('#app'))
 
-import app from './app.vue'
+// import app from './app.vue'
 // import app from './app-mobile.vue'
-// import app from './app-mobile-using-oui.vue'
+import app from './app-mobile-using-oui.vue'
 
 createApp(app).mount('#app')
