44cfca9372e6e5d29cf69c1201fcbc65db0ddb65
Dirk Holtwick <dirk.holtwick@gmail.com>
2024-05-27 10:01:45 +0200

    fix: demo more mobile friendly

--- src/app.vue
+++ src/app.vue
@@ -60,21 +60,21 @@ function toggleDark() {
         </template>
       </select>
 
-      <div class="_space" />
+      <!-- <div class="_space" />
 
       <div class="_middle">
         <OuiCheckbox v-model="dark" switch tooltip="Show light and dark side by side">
           dual
         </OuiCheckbox>
-      </div>
+      </div> -->
 
       <div class="_space" />
 
-      <div class="_middle">
+      <!-- <div class="_middle">
         v{{ pkg.version }}
-      </div>
+      </div> -->
 
-      <OuiButton @click="toggleDark">
+      <OuiButton @click="toggleDark" @dblclick="dark = !dark">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-sun-moon"><path d="M12 8a2.83 2.83 0 0 0 4 4 4 4 0 1 1-4-4" /><path d="M12 2v2" /><path d="M12 20v2" /><path d="m4.9 4.9 1.4 1.4" /><path d="m17.7 17.7 1.4 1.4" /><path d="M2 12h2" /><path d="M20 12h2" /><path d="m6.3 17.7-1.4 1.4" /><path d="m19.1 4.9-1.4 1.4" /></svg>
       </OuiButton>
 
