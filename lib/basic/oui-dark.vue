79dd9dd4664b128cccf80e2bdb7fd45d7ff264d5
Dirk Holtwick <dirk.holtwick@gmail.com>
2024-05-29 10:34:03 +0200

    feat: oui-dark

---
+++ lib/basic/oui-dark.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { useDark, useToggle } from '@vueuse/core'
+
+// TODO
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+</script>
+
+<template>
+  <button class="oui-dark" @click="toggleDark()">
+    toggleDark {{ isDark }}
+  </button>
+</template>
