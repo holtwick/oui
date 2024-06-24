90c3d2e162eab6df10f2df464e5ddedee302eb51
Dirk Holtwick <dirk.holtwick@gmail.com>
2024-06-24 16:41:25 +0200

    feat: oui-string lazy

---
+++ lib/basic/oui-input.demo.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+import { reactive } from 'vue'
+import { OuiCheckbox, OuiDemo, OuiInput, OuiSelect } from '../lib'
+
+const state = reactive({
+  value: '',
+  lazy: true,
+  required: false,
+  title: 'Title',
+  type: 'text',
+})
+</script>
+
+<template>
+  <div>
+    <OuiInput
+      v-model="state.value"
+      :lazy="state.lazy"
+      :required="state.required"
+      :title="state.title"
+      :type="state.type"
+    />
+  </div>
+
+  <OuiDemo :state="state">
+    <OuiInput v-model="state.value" title="value" />
+    <OuiInput v-model="state.title" title="title" />
+    <OuiCheckbox v-model="state.lazy" switch title="lazy" />
+    <OuiCheckbox v-model="state.required" switch title="required" />
+    <OuiSelect v-model="state.type" title="type" :options="['text', 'url', 'email', 'tel', 'search']" />
+  </ouidemo>
+</template>
--- lib/basic/oui-input.vue
+++ lib/basic/oui-input.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import { type InputTypeHTMLAttribute, computed, reactive, ref, watch } from 'vue'
 import OuiFormItem from './oui-form-item.vue'
 
 import './oui-form.styl'
@@ -7,17 +8,42 @@ defineOptions({
   inheritAttrs: false,
 })
 
-withDefaults(defineProps<{
+// type InputTypeHTMLAttribute = 'button' | 'checkbox' | 'color' | 'date' | 'datetime-local' | 'email' | 'file' | 'hidden' | 'image' | 'month' | 'number' | 'password' | 'radio' | 'range' | 'reset' | 'search' | 'submit' | 'tel' | 'text' | 'time' | 'url' | 'week' | (string & {});
+
+const props = withDefaults(defineProps<{
   title?: string
   description?: string
   required?: boolean
-  type?: 'text' | 'url' | 'email' | 'tel' | 'search'
+  type?: InputTypeHTMLAttribute // 'text' | 'url' | 'email' | 'tel' | 'search'
   id?: string
+  lazy?: boolean
 }>(), {
   type: 'text',
 })
 
 const model = defineModel({ required: true })
+
+const value = ref('')
+
+watch(
+  value,
+  (v) => {
+    if (!props.lazy)
+      model.value = value
+  },
+  { immediate: true })
+
+watch(() => model.value,
+  (v) => {
+    value.value = v
+  },
+  { immediate: true })
+
+function lazyUpdate() {
+  if (props.lazy) {
+    model.value = value.value
+  }
+}
 </script>
 
 <template>
@@ -29,10 +55,12 @@ const model = defineModel({ required: true })
   >
     <input
       :id="id"
-      v-model="model"
+      v-model="value"
       :type="type"
       class="oui-input oui-input-string"
       v-bind="$attrs"
+      @blur="lazyUpdate"
+      @keypress.enter="lazyUpdate"
     >
   </OuiFormItem>
 </template>
