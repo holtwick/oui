0185ae3974622a86b18f2155d5683605afa5c08a
Dirk Holtwick <dirk.holtwick@gmail.com>
2024-07-04 10:34:49 +0200

    feat: demo of useSignal binding

---
+++ lib/basic/signal.demo.vue
@@ -0,0 +1,41 @@
+<script lang="ts" setup>
+import type { Ref, UnwrapRef } from 'vue'
+import { onUnmounted, ref, watch } from 'vue'
+import type { LoggerInterface, Signal } from 'zeed'
+import { Logger, useSignal } from 'zeed'
+import { OuiButton, OuiCheckbox, OuiText } from '@/lib'
+
+const log: LoggerInterface = Logger('test-signal')
+
+function useSignalRef<T>(signal: Signal<T>, readwrite = true): Ref<UnwrapRef<T>> {
+  const signalVue = ref<T>(signal.get())
+  onUnmounted(signal.on(v => signalVue.value = v as any))
+  if (readwrite)
+    watch(signalVue, v => signal.set(v as any))
+  return signalVue
+}
+
+// Connect
+const signal = useSignal(true)
+const signalVue = useSignalRef(signal)
+
+onUnmounted(signal.on(v => log(`new signal value = ${v}`)))
+</script>
+
+<template>
+  <OuiText>
+    <h1>useSignal</h1>
+    <p>Demonstration of a bidirectional binding between zeed framework <tt>useSignal</tt> and a Vue reactive value. More of academic interest than practical one ;) </p>
+
+    <p>
+      <OuiCheckbox v-model="signalVue" type="checkbox" switch />
+      Represent signalVue: <tt>{{ signalVue }}</tt>
+    </p>
+
+    <p>
+      <OuiButton @click="signal.set(!signal.get())">
+        toggle signal
+      </OuiButton>
+    </p>
+  </OuiText>
+</template>
