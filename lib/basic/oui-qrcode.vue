15f8e9010746d332f8844a6a64a4bb8ea4697ddd
Dirk Holtwick <dirk.holtwick@gmail.com>
2024-09-23 13:43:41 +0200

    feat: oui-qrcode

---
+++ lib/basic/oui-qrcode.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import { computed } from 'vue'
+import { Logger } from 'zeed'
+import { useQRCode } from './qrcode'
+
+const props = defineProps<{
+  content: string
+}>()
+
+const qrcode = useQRCode()
+
+const qrcodeHTML = computed(() => {
+  const typeNumber = 0
+  const errorCorrectionLevel = 'M'
+  const qr = qrcode(typeNumber, errorCorrectionLevel) as any
+
+  qr.addData(props.content ?? '')
+  qr.make()
+  return qr.createSvgTag({
+    scalable: true,
+  })
+})
+</script>
+
+<template>
+  <div class="oui-qrcode" v-html="qrcodeHTML" />
+</template>
