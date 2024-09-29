bb10f4df72734c74d5c75139acc786563cd08948
Dirk Holtwick <dirk.holtwick@gmail.com>
2024-09-29 12:24:31 +0200

    feat: oui-date

---
+++ lib/basic/oui-date.vue
@@ -0,0 +1,40 @@
+<script lang="ts" setup>
+import { computed } from 'vue'
+import { dayFromString, dayFromToday, dayToString, getTimestamp } from 'zeed'
+import OuiFormItem from './oui-form-item.vue'
+
+import './oui-form.styl'
+
+withDefaults(defineProps<{
+  title?: string
+  description?: string
+  required?: boolean
+  id?: string
+}>(), {
+})
+
+const model = defineModel<number | undefined>({
+  required: true,
+  default: dayFromToday(),
+})
+
+const date = computed({
+  get() {
+    return model.value ? dayToString(model.value) : ''
+  },
+  set(value) {
+    model.value = value ? dayFromString(value) as number : undefined
+  },
+})
+</script>
+
+<template>
+  <OuiFormItem
+    :id="id"
+    :title="title"
+    :description="description"
+    :required="required"
+  >
+    <input :id="id" v-model="date" type="date" class="oui-input oui-input-datetime">
+  </OuiFormItem>
+</template>
