29a02a213c3af00d0117b7d54a7ebcee60c4dd29
Dirk Holtwick <dirk.holtwick@gmail.com>
2024-10-05 19:25:57 +0200

    feat: legacy font size step

---
+++ src/css-utopia-space.demo.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import './css-utopia-space.styl'
+</script>
+
+<template>
+  <div class="_utopia_space">
+    <div class="_font--2">
+      _font--2
+    </div>
+
+    <div class="_font--1">
+      _font--1
+    </div>
+
+    <div class="_font-0">
+      _font-0
+    </div>
+
+    <div class="_font-1">
+      _font-1
+    </div>
+
+    <div class="_font-2">
+      _font-2
+    </div>
+
+    <div class="_font-3">
+      _font-3
+    </div>
+
+    <div class="_font-4">
+      _font-4
+    </div>
+
+    <div class="_font-5">
+      _font-5
+    </div>
+  </div>
+</template>
