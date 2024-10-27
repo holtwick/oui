d575b0040d2bfb03f5de6de419212181d04b36c4
Dirk Holtwick <dirk.holtwick@gmail.com>
2024-10-27 18:49:30 +0100

    fix: stab. oui-modile

--- lib/mobile/oui-mobile.vue
+++ lib/mobile/oui-mobile.vue
@@ -22,73 +22,84 @@ const keyboardAnimationDuration = 400
 
 if (useSingleton('oui-mobile')) {
   log('init')
+
   if (window.visualViewport != null) {
     let timer: any
     let virtualKeyboardActive = false
 
     async function resizeHandler() {
-    // Adjust the height!
-      const height = `${window.visualViewport?.height.toString()}px`
-      log(`resize height=${height}`)
-      // document.documentElement.style.height = `${window.visualViewport?.height.toString()}px`
-      document.documentElement.style.setProperty('--visible-height', height)
+      try {
+      // Adjust the height!
+        const height = `${window.visualViewport?.height.toString()}px`
+        log(`resize height=${height}`)
+        // document.documentElement.style.height = `${window.visualViewport?.height.toString()}px`
+        document.documentElement.style.setProperty('--visible-height', height)
 
-      await nextTick()
+        await nextTick()
 
-      // Try to guess, if the virtual keyboard did show up
-      // https://stackoverflow.com/questions/497094/how-do-i-find-out-which-dom-element-has-the-focus
-      virtualKeyboardActive = document.activeElement?.matches('input,textarea,[contenteditable]') ?? false
-      if (virtualKeyboardActive) {
-        document.documentElement.classList.add('virtual-keyboard')
-        clearTimeout(timer)
+        // Try to guess, if the virtual keyboard did show up
+        // https://stackoverflow.com/questions/497094/how-do-i-find-out-which-dom-element-has-the-focus
+        virtualKeyboardActive = document.activeElement?.matches('input,textarea,[contenteditable]') ?? false
+        if (virtualKeyboardActive) {
+          document.documentElement.classList.add('virtual-keyboard')
+          clearTimeout(timer)
 
-        // With some delay scroll active/focussed element into view
-        timer = setTimeout(() => {
-          if (document.activeElement?.matches('input,textarea,[contenteditable]')) {
-            log('scroll into view', document.activeElement)
-            document.activeElement?.scrollIntoView({
-            // behavior: 'smooth',
-              behavior: 'instant',
-              block: 'nearest',
-              inline: 'nearest',
-            })
-          }
-        }, keyboardAnimationDuration)
+          // With some delay scroll active/focussed element into view
+          timer = setTimeout(() => {
+            if (document.activeElement?.matches('input,textarea,[contenteditable]')) {
+              log('scroll into view', document.activeElement)
+              document.activeElement?.scrollIntoView({
+                // behavior: 'smooth',
+                behavior: 'instant',
+                block: 'nearest',
+                inline: 'nearest',
+              })
+            }
+          }, keyboardAnimationDuration)
+        }
+        else {
+          document.documentElement.classList.remove('virtual-keyboard')
+        }
       }
-      else {
-        document.documentElement.classList.remove('virtual-keyboard')
+      catch (err) {
+        log.error('resizeHandler issue', err)
       }
     }
 
-    useEventListener(window.visualViewport, 'resize', resizeHandler)
-    useEventListener(window.visualViewport, 'scroll', resizeHandler)
+    try {
+      useEventListener(window.visualViewport, 'resize', resizeHandler)
+      useEventListener(window.visualViewport, 'scroll', resizeHandler)
 
-    // if (props.mode === 'app') {
-    // Intercept `touchmove` where no scrolling is planned in our UI
-    useEventListener(window, 'touchmove', (ev) => {
-      log('touch move', ev.target)
+      // if (props.mode === 'app') {
+      // Intercept `touchmove` where no scrolling is planned in our UI
+      useEventListener(window, 'touchmove', (ev) => {
+        log('touch move', ev.target)
 
-      // if (virtualKeyboardActive === false)
-      //   return
+        // if (virtualKeyboardActive === false)
+        //   return
 
-      if (isInsideScrollable(ev.target as any))
-        return
+        if (isInsideScrollable(ev.target as any))
+          return
 
-      log('prevent scroll')
-      // If not avoid scrolling
-      ev.preventDefault()
+        log('prevent scroll')
+        // If not avoid scrolling
+        ev.preventDefault()
       // ev.stopPropagation()
-    }, {
+      }, {
       // make sure, we get those events
-      passive: false,
-      // capture them as early as possible
-      capture: true,
-    })
-    // }
+        passive: false,
+        // capture them as early as possible
+        capture: true,
+      })
+      // }
 
-    onMounted(resizeHandler)
+      onMounted(resizeHandler)
+    }
+    catch (err) {
+      log.error('init issue', err)
+    }
   }
-
+  
   onMounted(() => document.documentElement.classList.add('oui-mobile'))
 }
 </script>
