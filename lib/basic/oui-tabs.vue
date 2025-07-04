390a8c992bba27697e35ccd55596893ae209d89f
Dirk Holtwick <dirk.holtwick@gmail.com>
2025-07-04 12:41:57 +0200

    demos and slots

--- lib/basic/oui-tabs.vue
+++ lib/basic/oui-tabs.vue
@@ -24,10 +24,12 @@ const name = computed<string>(() => modelValue.value ?? props.tabs?.[0]?.name ??
       <template #default="{ options, updateModelValue }">
         <template v-for="tab in options" :key="tab.name">
           <button :class="{ _active: modelValue === tab.name }" @click="updateModelValue(tab.name)">
-            <template v-if="tab.icon">
-              <component :is="tab.icon" />
-            </template>
-            {{ tab.title ?? tab.name }}
+            <slot :name="`tab-${tab.name}`" v-bind="{ tab }">
+              <template v-if="tab.icon">
+                <component :is="tab.icon" />
+              </template>
+              {{ tab.title ?? tab.name }}
+            </slot>
           </button>
         </template>
       </template>
