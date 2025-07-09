24b7ba4e3d3b07a3de71ef07fb01728ad994bf22
Dirk Holtwick <dirk.holtwick@gmail.com>
2025-07-09 10:01:39 +0200

    add oui-debug component with styling and demo

---
+++ lib/basic/oui-debug.demo.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import { reactive } from 'vue'
+import { OuiCheckbox, OuiDebug, OuiDemo, OuiText } from '@/lib'
+
+const state = reactive({
+  show: true,
+})
+</script>
+
+<template>
+  <OuiText>
+    <h1>oui-debug</h1>
+    <p>Move your mouse over the small red dot in the bottom right corner to open the debug panel. This is a quick way to debug some data.</p>
+    <OuiDebug v-if="state.show">
+      {{ state }}
+    </OuiDebug>
+  </OuiText>
+
+  <OuiDemo :state="state">
+    <OuiCheckbox v-model="state.show" switch title="show" />
+  </OuiDemo>
+</template>
