f4ce7bf0e49fc62f5f4d75e5d1ad1c47cc4ac88a
Dirk Holtwick <dirk.holtwick@gmail.com>
2025-10-07 09:44:59 +0200

    fix: card layout

--- lib/basic/oui-card.demo.vue
+++ lib/basic/oui-card.demo.vue
@@ -1,9 +1,24 @@
 <script lang="ts" setup>
+import { reactive } from 'vue'
 import { OuiCard } from '@/lib'
+import OuiCheckbox from './oui-checkbox.vue'
+import OuiInput from './oui-input.vue'
+
+const state = reactive({
+  checked: false,
+  text: 'Some text',
+})
 </script>
 
 <template>
-  <OuiCard title="Sample Card">
-    Hello
-  </OuiCard>
+  <div class="-y -gap">
+    <OuiCard title="Sample Card">
+      Hello
+    </OuiCard>
+
+    <OuiCard title="Sample Card">
+      <OuiInput v-model="state.text" label="Text input" title="Sample" description="Some text" />
+      <OuiCheckbox v-model="state.checked" label="Check me" title="Sample" description="Some text" />
+    </OuiCard>
+  </div>
 </template>
